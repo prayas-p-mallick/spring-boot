39581e74e1aabd663aee8f4e87b9f4e01ebd5e28
Prayas Preetam Mallick <prayaspreetammallick@outlook.com>
2024-07-10 19:39:41 +0530

    Initial Spring Boot Project Setup.

---
+++ week01-alice-and-cake-bakery/src/main/java/org/paradox/week01_alice_and_cake_bakery/AliceCakeBakeryApplication.java
@@ -0,0 +1,13 @@
+package org.paradox.week01_alice_and_cake_bakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceCakeBakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceCakeBakeryApplication.class, args);
+	}
+
+}
---
+++ week01-alice-and-cake-bakery/src/test/java/org/paradox/week01_alice_and_cake_bakery/AliceCakeBakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paradox.week01_alice_and_cake_bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceCakeBakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
