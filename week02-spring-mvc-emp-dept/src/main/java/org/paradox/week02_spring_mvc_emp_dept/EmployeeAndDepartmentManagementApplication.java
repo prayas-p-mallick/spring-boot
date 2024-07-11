9e93022ecb40d73c3c1a6b29bad96aec11751743
Prayas Preetam Mallick <prayaspreetammallick@outlook.com>
2024-07-11 15:19:32 +0530

    Initial Spring Boot Project Setup.

---
+++ week02-spring-mvc-emp-dept/src/main/java/org/paradox/week02_spring_mvc_emp_dept/EmployeeAndDepartmentManagementApplication.java
@@ -0,0 +1,13 @@
+package org.paradox.week02_spring_mvc_emp_dept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAndDepartmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAndDepartmentManagementApplication.class, args);
+	}
+
+}
---
+++ week02-spring-mvc-emp-dept/src/test/java/org/paradox/week02_spring_mvc_emp_dept/EmployeeAndDepartmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paradox.week02_spring_mvc_emp_dept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAndDepartmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
